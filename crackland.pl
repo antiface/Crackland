249db46e60b38d7de44beb56f327e9d55d7f988c
A.G. <gagnon.alex@gmail.com>
2014-09-23 12:29:12 -0400

    Create crackland.pl

---
+++ crackland.pl
@@ -0,0 +1,59 @@
+novel(crackland).
+character(alphonse, crackland).
+character(evelyn, crackland).
+character(jgdufray, crackland).
+character(drkropotkin, crackland).
+character(philipsoutine, crackland).
+character(baron, crackland).
+character(boy, crackland).
+character(maud, crackland).
+city(montreal).
+city(venice).
+city(stpetersburg).
+place(crackland).
+place(stelsewhere).
+place(montreal).
+place(stpetersburg).
+place(assembly).
+place(house)
+place(houseof)
+place(room)
+template(city).
+template(cityof).
+template(houseof).
+painter(alphonse).
+doctor(drkropotkin).
+doctor(evelyn).
+male(alphonse).
+male(jgdufray).
+male(drkropotkin).
+male(philipsoutine).
+female(evelyn).
+female(maud).
+organization(assembly).
+organization(historyproject).
+project(historyproject).
+title(historyproject).
+title(crackland).
+hero(alphonse).
+author(alex).
+author(alphonse).
+author(evelyn).
+author(jgdufray).
+author(drkropotkin).
+philosophy(habitance).
+science(historiotherapeusis).
+psychiatrist(drkropotkin).
+professor(evelyn).
+professor(philipsoutine).
+radiodj(jgdufray).
+phenomenology(crackland).
+complexsystem(crackland).
+complexsystem(historyproject).
+failure(historyproject).
+addict(alphonse).
+addict(jgdufray).
+girlfriend(evelyn, alphonse).
+boyfriend(alphonse, evelyn).
+ex(evelyn, alphonse).
+ex(alphonse, evelyn).
