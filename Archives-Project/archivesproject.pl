f989d35a27eef0043bf270223e82d47bd54cddbf
A.G <gagnon.alex@gmail.com>
2019-11-07 12:27:24 -0500

    Create archivesproject.pl

---
+++ Archives-Project/archivesproject.pl
@@ -0,0 +1,101 @@
+novel(archivesproject).
+novel(historyproject).
+prequel(historyproject).
+sequel(archivesproject).
+character(alphonse, novel).
+character(evelyn, novel).
+character(jgdufray, novel).
+character(drkropotkin, novel).
+character(philipsoutine, novel).
+character(baron, novel).
+character(boy, novel).
+character(maud, novel).
+city(montreal).
+city(venice).
+city(stpetersburg).
+place(crackland).
+place(constructionland).
+place(cland).
+place(nous).
+place(antifacecloud).
+place(stelsewhere).
+place(montreal).
+place(stpetersburg).
+place(assembly).
+place(house).
+place(houseof).
+place(room).
+place(city).
+template(city).
+template(cityof).
+template(houseof).
+painter(alphonse).
+doctor(drkropotkin).
+doctor(evelyn).
+male(alphonse).
+male(jgdufray).
+male(drkropotkin).
+male(philipsoutine).
+female(evelyn).
+female(maud).
+organization(assembly).
+organization(historyproject).
+project(historyproject).
+project(archivesproject).
+title(historyproject).
+title(archivesproject).
+hero(alphonse).
+antihero(alphonse).
+author(alex).
+author(alphonse).
+author(evelyn).
+author(jgdufray).
+author(drkropotkin).
+philosophy(habitance).
+science(historiotherapeusis).
+science(historiomics).
+science(signalscience).
+science(archivalscience).
+science(actuarialscience).
+psychiatrist(drkropotkin).
+professor(evelyn).
+professor(philipsoutine).
+radiodj(jgdufray).
+phenomenology(historyproject).
+phenomenology(archivesproject).
+phenomenology(novel).
+system(novel).
+system(archivesproject).
+complexsystem(archivesproject).
+complexsystem(historyproject).
+failure(historyproject).
+addict(alphonse).
+addict(jgdufray).
+girlfriend(evelyn, alphonse).
+boyfriend(alphonse, evelyn).
+ex(evelyn, alphonse).
+ex(alphonse, evelyn).
+historyproject(project).
+historyproject(action).
+historyproject(behavior).
+operation(historyproject).
+operation(archivesproject).
+operation(artoperation).
+operation(artop).
+method(design).
+method(iterateddesign).
+process(historion).
+theory(noisefield).
+theory(design).
+theme(risk).
+theme(logistics).
+theme(erasure).
+format(text).
+format(image).
+format(sound).
+medium(text).
+medium(image).
+medium(sound).
+composition(novel).
+composition(archivesproject).
+composition(historyproject).
